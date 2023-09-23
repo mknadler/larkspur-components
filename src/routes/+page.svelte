51269156bfe6e695ca1b53881d71344d013c6100
mknadler <miriam.k.nadler@gmail.com>
2023-09-23 02:39:43 -0400

    Add gridlink component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Checkbox from '$lib/components/Checkbox.svelte'
 	import RadioGroup from '$lib/components/RadioGroup.svelte'
+	import GridLink from '$lib/components/GridLink.svelte'
 	import Logo from './logo.svelte'
 
 	let veryHackyWayToUseThisStringUntilIInstallAMarkdownThingy = `import { Checkbox } from 'larkspur';`
@@ -41,6 +42,15 @@
 	Reactive data: {radiogroupBound}
 </section>
 
+<section>
+	<h2>Grid link</h2>
+	<GridLink magnitude={2} numPoints={20} duration={3000}>
+		<a slot="link" href="#">This is a link</a>
+	</GridLink>
+	<GridLink magnitude={10} numPoints={30} duration={5000}>
+		<a slot="link" href="#">This is a different link</a>
+	</GridLink>
+</section>
 
 
 
